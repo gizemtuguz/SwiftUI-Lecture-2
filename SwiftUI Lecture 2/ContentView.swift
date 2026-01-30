72a5b5f2df2849c4a0067db3ff5be989dff735dd
gizemtuguz <gizemtuguz@ogr.eskisehir.edu.tr>
2026-01-30 23:49:40 +0300

    Initial commit - SwiftUI Lecture 2

--- SwiftUI Lecture 2/ContentView.swift
+++ SwiftUI Lecture 2/ContentView.swift
@@ -9,16 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack {  //bag of lego view
+            Image(systemName: "pawprint.circle.fill")
+                .foregroundStyle(.blue)
+                //.font(.largeTitle) // circle etkilenmiyor fakat emojiler etkileniyor hatta büyük harf boyutunda oluyor.
+            Text("SwiftUI Lecture 2!")
+                .background(.gray)  //önce background belirleyip sonra padding eklediğimizde background paddingden bğımsız bir şekilde oluyor yani order matters
+                .padding()
+                //.font(.largeTitle)
+                .foregroundStyle(.green)
+            Text("Hellooo!")
+                .padding()
+                .background(.blue)
+                .font(.caption) //default font largetitle olarajk yazılsa bile override edilebilir
+            Circle()
+                //.font(.largeTitle) //circle bir view olduğu için bu fonksiyon mevcut fakat no effect
         }
+        .font(.largeTitle) //vstackteki tüm viewlara uyguluyor.
         .padding()
     }
 }
 
-#Preview {
+#Preview {   //önizlemek için gerekli
     ContentView()
 }
